2408e3748a97661eea7c628b5c19eb8f2ac161fd
drighetto <drighetto@excellium-services.com>
2021-10-27 08:13:58 +0200

    setup build

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ mkdir build 2>/dev/null
 export GOARCH=amd64
 for os in ${allOS[@]}; do
     export GOOS=$os
+    if [ "$os" == "windows" ];
+    then
+        export GOEXE=.exe
+    fi
     bin_name="pkcheck-$GOOS-$GOARCH$GOEXE"
     go build -o build/$bin_name
     ls -l build/$bin_name
